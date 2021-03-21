6172a9a44034762cc1e8ce634735565541dd0267
vbazanella <vinicius.bazanella1@gmail.com>
2021-03-21 15:59:00 -0300

    Finaliza a implementaçao do tcp.

--- tcp/client.js
+++ tcp/client.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 const client = new net.Socket();
 
 client.connect(1234, '127.0.0.1', function () {
-    console.log('Connected');
+    console.log('Cliente conectado.');
 
     const data = fs.readFileSync('clientdata.txt')
     client.write(data)
+    console.log('Mensagem enviada ao servidor.\n')
 });
 
 client.on('data', function (data) {
     const message = data.toString()
-    console.log(`Servidor diz: ${message}`);
+    console.log(` > Servidor diz: ${message}`);
 });
 
 client.on('close', function () {
--- tcp/server.js
+++ tcp/server.js
@@ -1,26 +1,40 @@
 const net = require('net');
 const fs = require('fs');
-const isEven = require('../utils/isEven');
 
 const server = net.createServer(socket => {
     socket.on('data', (data) => {
         const message = data.toString()
-        console.log(`Mensagem do cliente mandou: ${message}`)
+        console.log(` > Cliente diz: ${message}`)
 
         setTimeout(() => {
-            const data = fs.readFileSync('serverdata.txt')
-            socket.write(data)
+            const response = fs.readFileSync('serverdata.txt')
+            console.log('Dados enviados ao cliente.\n')
+            socket.write(response)
             socket.pipe(socket)
         }, 1000)
     })
 });
 
-server.listen(1234, '127.0.0.1');
+server.listen(1234, 'localhost');
+
+server.on('listening', () => {
+    const { port, address, family } = server.address();
+
+    console.log(`Servidor escutando na porta ${port}`);
+    console.log(`IP do servidor: ${address}`);
+    console.log(`Familia do servidor é ${family}\n`);
+});
 
 server.on('close', () => {
     console.log('Servidor fechado.')
 })
 
+server.on('error', error => {
+    console.log('Error: ' + error);
+    server.close();
+});
+
+// servidor ficará aberto somente por 5 minutos.
 setTimeout(() => {
     server.close()
-}, 5 * 60 * 1000) // servidor fica aberto apenas por 5 minutos.
+}, 5 * 60 * 1000)
