e80968d0b0671e7514bbf3ce8807d4f5e9bf0402
vbazanella <vinicius.bazanella1@gmail.com>
2021-03-21 16:03:28 -0300

    Finaliza a implementaçao do modelo udp

--- udp/client.js
+++ udp/client.js
@@ -1,24 +1,20 @@
-
 const udp = require('dgram');
 const fs = require('fs');
 
-// cria um socket para o cliente
 const client = udp.createSocket('udp4');
 
-// buffer da mensagem a ser enviada
-// const data = Buffer.from('Olá, socket!');
 const data = fs.readFileSync('clientdata.txt')
 
-client.on('message', function (msg, info) {
-    console.log(`Resposta do servidor: ${msg.toString()}`);
-    console.log(`Origem: ${info.address}:${info.port} \nTamanho dos dados: ${msg.length} \n`);
-});
-
-// enviando mensagem
 client.send(data, 2222, 'localhost', function (error) {
     if (error) {
+        console.log(error)
         client.close();
     } else {
         console.log('Mensagem enviada ao servidor.\n');
     }
+});
+
+client.on('message', function (data, _) {
+    const message = data.toString()
+    console.log(` > Servidor diz: ${message}`);
 });
--- udp/server.js
+++ udp/server.js
@@ -1,43 +1,35 @@
 const udp = require('dgram');
 const fs = require('fs');
 
-// instancia um servidor UDP
 const server = udp.createSocket('udp4');
 
-// emite em caso de erro
-server.on('error', function (error) {
-    console.log('Error: ' + error);
-    server.close();
+server.on('message', function (data, info) {
+    const message = data.toString()
+    console.log(` > Cliente diz: ${message}`);
+
+    setTimeout(() => {
+        const response = fs.readFileSync('serverdata.txt')
+        server.send(response, info.port, 'localhost', error => {
+            console.log('Mensagem respondida ao cliente.\n');
+        });
+    }, 1000)
 });
 
-// emite cada vez que o cliente manda algum dado
-server.on('message', function (msg, info) {
-    console.log('Mensagem recebida do cliente: ' + msg.toString());
-    console.log(`Origem: ${info.address}:${info.port} \nTamanho dos dados: ${msg.length} \n`);
-
-    // envia mensagem
-    const response = fs.readFileSync('serverdata.txt')
-    server.send(response, info.port, 'localhost', function (error) {
-        if (error) {
-            client.close();
-        } else {
-            console.log('Mensagem respondida ao cliente.');
-        }
-    });
-});
-
-// emite quando o socket está pronto, esperando por mensagens
 server.on('listening', function () {
-    const {port, address, family} = server.address();
-    
+    const { port, address, family } = server.address();
+
     console.log(`Servidor escutando na porta ${port}`);
     console.log(`IP do servidor: ${address}`);
     console.log(`Familia do servidor é ${family}\n`);
 });
 
-// emite quando o socket é fechado
 server.on('close', function () {
     console.log('Socket fechado.');
+});
+
+server.on('error', function (error) {
+    console.log('Error: ' + error);
+    server.close();
 });
 
 server.bind(2222);
